1fb238a3d77559f019964b1e5132435094e62eee
Joe Jankowiak <joe@ascenddigitalexperts.com>
2026-09-17 19:10:37 -0400

    fix: settings page layout

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 
 function Tabs({
   className,
-  orientation = "vertical",
+  orientation = "horizontal",
   ...props
 }: TabsPrimitive.Root.Props) {
   return (
@@ -17,6 +17,7 @@ function Tabs({
       orientation={orientation}
       className={cn(
         "group/tabs flex gap-2 data-horizontal:flex-col",
+        orientation === "horizontal" && "flex-col",
         className
       )}
       {...props}
@@ -25,7 +26,7 @@ function Tabs({
 }
 
 const tabsListVariants = cva(
-  "group/tabs-list inline-flex w-fit items-center justify-center rounded-lg p-[3px] text-muted-foreground group-data-horizontal/tabs:h-8 group-data-vertical/tabs:h-fit group-data-vertical/tabs:flex-col data-[variant=line]:rounded-none",
+  "group/tabs-list inline-flex w-fit shrink-0 items-center justify-center self-start rounded-lg p-[3px] text-muted-foreground group-data-horizontal/tabs:h-8 group-data-vertical/tabs:h-fit group-data-vertical/tabs:flex-col data-[variant=line]:rounded-none",
   {
     variants: {
       variant: {
